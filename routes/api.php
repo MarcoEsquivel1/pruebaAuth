428cf8bf42171e7d832105f8c282ac8c6e1c81fd
MarcoEsquivel <marco.esquivel@catolica.edu.sv>
2022-09-14 11:20:40 -0600

    logout

--- routes/api.php
+++ routes/api.php
@@ -22,4 +22,5 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
 Route::apiResource('/cita', CitasController::class)->middleware('auth:api');
